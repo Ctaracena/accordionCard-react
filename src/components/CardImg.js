f54b312fa1b9f6ffcd31c92a4d7187ee59d2b0a2
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-12-08 11:11:21 -0600

    some bugs fixed and box added

--- src/components/CardImg.js
+++ src/components/CardImg.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import img from '../images/illustration-woman-online-desktop.svg'
-/* import box from '../images/illustration-box-desktop.svg' */
+import box from '../images/illustration-box-desktop.svg'
 import './styles/CardImg.css'
 
 const CardImg = () => {
     return (
         <div>
             <img src={img} alt="Woman Online" className="woman"/>
-            {/* <img src={box} alt="box" className="box"/> */}
+            {<img src={box} alt="box" className="box"/>}
         </div>
     )
 }
